2d2531a9d5827d8530f4a41a2111a212122b3870
Pranita Turrey <64246126+pranitaT@users.noreply.github.com>
2024-06-19 14:28:16 +0530

    added responsiveness for card text

--- taskwise/src/pages/Workspace/WorkspaceDetails.jsx
+++ taskwise/src/pages/Workspace/WorkspaceDetails.jsx
@@ -1,9 +1,10 @@
+// WorkspaceDetails.js
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { Box, Paper, Typography, InputBase, Divider, Tabs, Tab, Button, Stack } from '@mui/material';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Box, Paper, Typography, InputBase, Divider, Tabs, Tab, Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import SearchIcon from '@mui/icons-material/Search';
-import AddIcon from '@mui/icons-material/Add';
+import Stack from "@mui/material/Stack";
 import Modal from "@mui/material/Modal";
 import WorkspaceTasks from './WorkspaceTasks';
 import WorkspaceSettings from './WorkspaceSettings';
@@ -46,116 +47,100 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
   borderRadius: theme.shape.borderRadius,
-  backgroundColor: '#f0f0f0',
+  backgroundColor: "#f0f0f0",
   marginLeft: 0,
-  width: '200px',
+  width: '120px',
   height: '30px',
   display: 'flex',
   alignItems: 'center',
-  transition: theme.transitions.create('width'),
-  '&:focus-within': {
-    width: '300px',
-  },
 }));
 
 const CustomBox = styled(Box)(({ theme }) => ({
   width: '100%',
   height: 'calc(100vh - 160px)',
+  overflowY: 'auto',
   padding: theme.spacing(2),
-  // overflowY: 'auto',
-  // '&::-webkit-scrollbar': {
-  //   width: '8px',
-  // },
-  // '&::-webkit-scrollbar-thumb': {
-  //   backgroundColor: '#888',
-  //   borderRadius: '4px',
-  // },
-  // '&::-webkit-scrollbar-thumb:hover': {
-  //   backgroundColor: '#555',
-  // },
-  // '-ms-overflow-style': 'none',
-  // 'scrollbar-width': 'thin',
-  // 'scrollbar-color': '#888 #e0e0e0',
-}));
-
-const NoWorkspacesMessage = styled('div')(({ theme }) => ({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '100%',
-  width: '100%',
-  fontSize: '1.5rem',
-  color: theme.palette.text.secondary,
+  '&::-webkit-scrollbar': {
+    display: 'none',
+  },
+  '-ms-overflow-style': 'none',
+  'scrollbar-width': 'none',
 }));
 
 function WorkspaceDetails() {
-    const { id } = useParams();
-    const dispatch = useDispatch();
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  const workspace = useSelector((state) => state.workspace.selectedWorkspace);
+  const projectData = useSelector((state) => state.workspace.selectedProjects);
+  const membersData = useSelector((state) => state.workspace.selectedMembers);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isFilter, setIsFilter] = useState(false);
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+    setIsFilter(true);
+  };
   
-    const workspace = useSelector((state) => state.workspace.selectedWorkspace);
-    const projectData = useSelector((state) => state.workspace.selectedProjects);
-    const membersData = useSelector((state) => state.workspace.selectedMembers);
-    const [isModalOpen, setModalOpen] = useState(false);
-    const [searchQuery, setSearchQuery] = useState("");
-    const [isFilter, setIsFilter] = useState(false);
-  
-    const handleSearchChange = (event) => {
-      setSearchQuery(event.target.value);
-      setIsFilter(true);
-    };
-    
-    const filteredProjects = projectData.filter((project) =>
-      project.name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-  
-    const handleOpenModal = () => {
-      setModalOpen(true);
-    };
-  
-    const handleCloseModal = () => {
-      setModalOpen(false);
-    };
-  
-    const projectAddStatus = useSelector(
-      (state) => state.project.projectAddStatus
-    );
-  
-    useEffect(() => {
-      if (projectAddStatus === "fulfilled") {
-        toast.success("Project created successfully!");
-        dispatch(resetProjectAddStatus());
-      }
-      if(projectAddStatus==="rejected"){
-        toast.error("Project not added!");
-        dispatch(resetProjectAddStatus());
-      }
-      // eslint-disable-next-line
-    }, [projectAddStatus]);
-  
-    const [selectedTab, setSelectedTab] = useState(0);
-  
-    useEffect(() => {
-      if (id) {
-        dispatch(fetchWorkspaceByIdAsync(id));
-        dispatch(fetchWorkspaceProjectsAsync(id));
-        dispatch(fetchWorkspaceMembersAsync(id));
-      }
-    }, [dispatch, id]);
-  
-    const handleTabChange = (event, newValue) => {
-      setSelectedTab(newValue);
-      setSearchQuery(""); // Clear search input when tab changes
-      dispatch(fetchWorkspaceByIdAsync(id));
-    };
-  
-    const handleProjectCreated = () => {
-      dispatch(fetchWorkspaceProjectsAsync(id));
-      setSelectedTab(0); // Change to "Projects" tab after creating a project
-    };
-  
-    if (!workspace) {
-      return <Loading/>;
+  const filteredProjects = projectData.filter((project) =>
+    project.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  const projectAddStatus = useSelector(
+    (state) => state.project.projectAddStatus
+  );
+
+  useEffect(() => {
+    if (projectAddStatus === "fulfilled") {
+      toast.success("Project created successfully!");
+      dispatch(resetProjectAddStatus());
     }
+    if(projectAddStatus==="rejected"){
+      toast.error("Project not added!");
+      dispatch(resetProjectAddStatus());
+    }
+    // eslint-disable-next-line
+  }, [projectAddStatus]);
+
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchWorkspaceByIdAsync(id));
+      dispatch(fetchWorkspaceProjectsAsync(id));
+      dispatch(fetchWorkspaceMembersAsync(id));
+    }
+  }, [dispatch, id]);
+
+  const handleTabChange = (event, newValue) => {
+    setSelectedTab(newValue);
+    setSearchQuery(""); // Clear search input when tab changes
+    dispatch(fetchWorkspaceByIdAsync(id));
+  };
+
+  const handleProjectCreated = () => {
+    dispatch(fetchWorkspaceProjectsAsync(id));
+    setSelectedTab(0); // Change to "Projects" tab after creating a project
+  };
+
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/createai");
+  };
+
+  if (!workspace) {
+    return <Loading/>;
+  }
 
   return (
     <Box
@@ -174,29 +159,83 @@ function WorkspaceDetails() {
         sx={{
           width: '100%',
           height: 'auto',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
         }}
       >
         <ToastContainer />
         <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                <img src={workspace.imgUrl} alt="Workspace"
-                        style={{ borderRadius: '8px', width: '50px', height: "44px", padding: "12px" }} />
-              <Box>
-                  <Box sx={{ display: "flex", flexDirection: "column" }}>
-                      <Typography gutterBottom variant="h6" component="div" sx={{ pt: 1, fontSize: '0.8rem' }}>
-                          Workspace / <strong>Details</strong>
-                      </Typography>
-                      <Box>
-                          <Typography gutterBottom variant="h6" component="div" sx={{ fontSize: '1rem' }}>
-                          {workspace.name}
-                          </Typography>
-                      </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <img src={workspace.imgUrl} alt="Workspace"
+                 style={{ borderRadius: '8px', width: '50px', height: "44px", padding: "12px" }} />
+            <Box>
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <Typography gutterBottom variant="h6" component="div" sx={{ pt: 1, fontSize: '0.8rem' }}>
+                  Workspace / <strong>Details</strong>
+                </Typography>
+                <Box>
+                  <Typography gutterBottom variant="h6" component="div" sx={{ fontSize: '1rem', wordWrap: 'break-word' }}>
+                    {workspace.name}
+                  </Typography>
                 </Box>
               </Box>
             </Box>
+          </Box>
+          <Stack
+            spacing={{ xs: 1, md: 2 }}
+            direction={{ xs: 'column', md: 'row' }}
+            alignItems={{ xs: 'flex-start', md: 'center' }}
+            justifyContent="space-between"
+            sx={{ px: 2, py: 1 }}
+          >
+          {selectedTab === 0 && (
+            <Box width={{ xs: '100%', md: 'auto' }} mt={{ xs: 1, md: 0 }}>
+              <Search>
+                <SearchIconWrapper>
+                  <SearchIcon sx={{ color: 'gray' }} />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="projects…"
+                  inputProps={{ 'aria-label': 'search' }}
+                  value={searchQuery}
+                  onChange={handleSearchChange}
+                />
+              </Search>
+            </Box>
+          )}
+          <Button
+            variant="contained"
+            size="small"
+            sx={{
+              fontSize: { xs: "0.65rem", md: "0.70rem" },
+              padding: { xs: "4px 6px", md: "4px 8px" },
+              backgroundColor: "#00c6ff",
+              backgroundImage: "linear-gradient(120deg, #00c6ff, #8e71df)",
+              color: "#fff",
+              width: { xs: '100%', md: 'auto' },
+              mb: { xs: 1, md: 0 }, // Margin bottom for responsive layout
+            }}
+            onClick={handleButtonClick}
+          >
+            Create with AI
+          </Button>
+          <Button
+            variant="contained"
+            size="small"
+            sx={{
+              fontSize: { xs: "0.65rem", md: "0.70rem" },
+              padding: { xs: "4px 6px", md: "4px 8px" },
+              width: { xs: '100%', md: 'auto' },
+            }}
+            onClick={handleOpenModal}
+          >
+            New Project
+          </Button>
+        </Stack>
         </Box>
         <Divider />
-        <Box sx={{ display: 'flex', alignItems: 'center'}}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
           <Tabs value={selectedTab} onChange={handleTabChange} aria-label="workspace tabs">
             <Tab
               label={
