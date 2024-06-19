42ef089ec462b718a6790632ae1524d611306a4e
Pranita Turrey <64246126+pranitaT@users.noreply.github.com>
2024-06-20 01:42:55 +0530

    Update CustomToolbar.jsx

--- taskwise/src/pages/CalendarPage/CustomToolbar.jsx
+++ taskwise/src/pages/CalendarPage/CustomToolbar.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import { Button, ButtonGroup } from '@mui/material';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
-const CustomToolbar = ({ onNavigate }) => {
+const CustomToolbar = ({ label, onNavigate }) => {
+  const navigate = (action) => {
+    switch (action) {
+      case 'PREV':
+        onNavigate('PREV');
+        break;
+      case 'TODAY':
+        onNavigate('TODAY');
+        break;
+      case 'NEXT':
+        onNavigate('NEXT');
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
-    <ButtonGroup sx={{justifyContent: 'center'}}>
-      <Button onClick={() => onNavigate('PREV')}>Prev</Button>
-      <Button onClick={() => onNavigate('TODAY')}>Today</Button>
-      <Button onClick={() => onNavigate('NEXT')}>Next</Button>
-    </ButtonGroup>
+    <div style={{ display: 'flex', alignItems: 'center', padding: '10px' }}>
+      <div style={{ flex: 1, textAlign: 'left', cursor: 'pointer' }} onClick={() => onNavigate('TODAY')}>
+        <span style={{ fontWeight: 'bold' }}>{label}</span>
+      </div>
+      <ButtonGroup variant="text">
+        <Button onClick={() => navigate('PREV')} startIcon={<NavigateBeforeIcon />}></Button>
+        <Button onClick={() => navigate('TODAY')} >Today</Button>
+        <Button onClick={() => navigate('NEXT')} endIcon={<NavigateNextIcon />}></Button>
+      </ButtonGroup>
+    </div>
   );
 };
 
